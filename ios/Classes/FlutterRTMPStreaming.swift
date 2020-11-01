af4e7e3bed2f76a2078556a750f80fe758b9a069
Julian <dev.julian.kowalczuk@gmail.com>
2020-11-01 15:12:40 +0100

    Update max bitrate hardcoded value for iOS

--- ios/Classes/FlutterRTMPStreaming.swift
+++ ios/Classes/FlutterRTMPStreaming.swift
@@ -188,7 +188,7 @@ public class FlutterRTMPStreaming : NSObject {
 
 class MyRTMPStreamQoSDelagate: RTMPStreamDelegate {
     let minBitrate: UInt32 = 300 * 1024
-    let maxBitrate: UInt32 = 2500 * 1024
+    let maxBitrate: UInt32 = 6000 * 1024
     let incrementBitrate: UInt32 = 512 * 1024
     
     func didPublishSufficientBW(_ stream: RTMPStream, withConnection: RTMPConnection) {
